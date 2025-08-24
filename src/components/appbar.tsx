46d33229b52e1c2ff71dfc7e5b6e15c9bdfac367
Om Landge <omlandge0000@gmail.com>
2025-08-25 00:21:06 +0530

    changed header

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function Appbar(){
     return(
-        <div className="flex justify-between border-b p-2 items-center">
+        <div className="flex justify-between border-b p-2 px-6 items-center">
             <h1 className="text-2xl font-bold flex justify-center items-center gap-1"><Image src='/logo.png' width={30} height={30} alt="logo" />Codium</h1>
             <div><LogOutButton/></div>
         </div>
